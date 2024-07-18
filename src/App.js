07261c534d1f283cc26f3d1de75281127f4096ec
Carlos Ochoa <ochoa1604@gmail.com>
2024-07-18 12:59:42 -0600

    created first pages components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './index.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div>
-      
+      <h1>My Portfolio</h1>
     </div>
   );
 }
