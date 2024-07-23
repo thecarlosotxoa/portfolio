e39391439e8a995a82d812f6da1f3b16b036f979
Carlos Ochoa <ochoa1604@gmail.com>
2024-07-23 15:36:34 -0600

    Projects components and styles

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Hero2 from '../components/Hero2'
+import Pricing from '../components/Pricing'
 
 const Projects = () => {
   return (
     <div>
       <Navbar/>
       <Hero2 heading="Projects" text="Some of my work."/>
+      <Pricing/>
       <Footer/>
     </div>
   )
