7548d8a769da77f2fc11d69cd2692d8b3e9bfe23
Carlos Ochoa <ochoa1604@gmail.com>
2024-07-23 19:27:12 -0600

    workcard components

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/images/project1.png";
+import pro2 from "../assets/images/project2.png";
+import pro3 from "../assets/images/project3.png";
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Website designed to go through the best sold products in the past few days",
+        view: "www.google.com"
+    },
+    {
+        imgsrc: pro2,
+        title: "Ecommerce Website Design",
+        text: "Website designed to go through the best sold products in the past few days",
+        view: "www.google.com"
+    },
+    {
+        imgsrc: pro3,
+        title: "Ecommerce Website Design",
+        text: "Website designed to go through the best sold products in the past few days",
+        view: "www.google.com"
+    }    
+]
+
+export default ProjectCardData;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Hero2 from '../components/Hero2'
 import Pricing from '../components/Pricing'
+import Work from '../components/Work'
 
 const Projects = () => {
   return (
     <div>
       <Navbar/>
       <Hero2 heading="Projects" text="Some of my work."/>
+      <Work/>
       <Pricing/>
       <Footer/>
     </div>
